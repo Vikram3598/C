4396b0ee14075e604605b9ac4982c1afdfec6fd4
VikramGupta <vikram1998gupta@gmail.com>
2022-08-11 21:09:32 +0530

    tower of hanoi

---
+++ tower_of_hanoi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void TOH(int n,char x,char y,char z) {
+   if(n>0) {
+      TOH(n-1,x,z,y);
+      printf("\n%c to %c",x,y);
+      TOH(n-1,z,y,x);
+   }
+   printf("\n");
+}
+int main() {
+   int n=3;
+   TOH(n,'A','B','C');
+}
